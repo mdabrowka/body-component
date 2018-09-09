3d7b22e16b37448fa4479edaefd4bba619f153c7
Marta <martadabrowka@gmail.com>
2018-09-09 07:30:35 +0100

    initial commit: renders body

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BodyComponent } from './Components/BodyComponent';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <BodyComponent/>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/BodyComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import body from '../daysix-react-coding-challenge-assets/body-front.svg'
+
+export class BodyComponent extends Component {
+    render() {
+       return (<img src={body}/>)
+    }
+}
+
+export default BodyComponent
+
