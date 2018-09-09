1ca4c2d88383ba0e094a53c9083f57841bc62dc9
Marta <martadabrowka@gmail.com>
2018-09-09 08:46:52 +0100

    refactors BodyCompoment to import render

--- src/Components/BodyComponent.jsx
+++ src/Components/BodyComponent.jsx
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
-import body from '../daysix-react-coding-challenge-assets/body-front.svg';
-import bleeding from '../daysix-react-coding-challenge-assets/bleeding.svg';
-import bruise from '../daysix-react-coding-challenge-assets/bruise.svg';
-import fracture from '../daysix-react-coding-challenge-assets/fracture.svg';
-import laceration from '../daysix-react-coding-challenge-assets/laceration.svg';
+import renderFigure from '../HelperFunctions/DisplayHelpers';
 import './BodyComponent.css';
 
 export class BodyComponent extends Component {
 
     render() {
-       return (
-       <main>
-           <div>
-                <img alt="body" src={body}/>
-            </div>
-            <div> 
-                <ul id="injuries">
-                    <li><img class="injury-type" alt="bleeding" src={bleeding}/></li>
-                    <li><img class="injury-type" alt="bruise" src={bruise}/></li>
-                    <li><img class="injury-type" alt="fracture" src={fracture}/></li>
-                    <li><img class="injury-type" alt="laceration" src={laceration}/></li>
-                </ul>
-            </div>
-       </main>
-        )
+       return renderFigure();
     }
 }
 
---
+++ src/HelperFunctions/DisplayHelpers.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import body from '../daysix-react-coding-challenge-assets/body-front.svg';
+import bleeding from '../daysix-react-coding-challenge-assets/bleeding.svg';
+import bruise from '../daysix-react-coding-challenge-assets/bruise.svg';
+import fracture from '../daysix-react-coding-challenge-assets/fracture.svg';
+import laceration from '../daysix-react-coding-challenge-assets/laceration.svg';
+
+export const renderFigure = () => {
+    return (
+        <main>
+            <div>
+                 <img alt="body" src={body}/>
+             </div>
+             <div> 
+                 <ul id="injuries">
+                     <li><img class="injury-type" alt="bleeding" src={bleeding}/></li>
+                     <li><img class="injury-type" alt="bruise" src={bruise}/></li>
+                     <li><img class="injury-type" alt="fracture" src={fracture}/></li>
+                     <li><img class="injury-type" alt="laceration" src={laceration}/></li>
+                 </ul>
+             </div>
+        </main>
+         );
+    }
+
+export default renderFigure;
